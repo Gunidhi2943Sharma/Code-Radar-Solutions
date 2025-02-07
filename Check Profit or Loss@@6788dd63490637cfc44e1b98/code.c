ffe2bf23392d4d438fbc5af8bb9bd8f8e7b66a89
Gunidhi2943Sharma <gunidhi2943sharma@gmail.com>
2025-02-07 10:17:19 +0530

    Solution to Check Profit or Loss

--- Check Profit or Loss@@6788dd63490637cfc44e1b98/code.c
+++ Check Profit or Loss@@6788dd63490637cfc44e1b98/code.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-    float selling_price, cost_price;
-    scanf("%f %f", &selling_price, &cost_price);
-    if (selling_price>=cost_price)
+    int selling_price, cost_price;
+    scanf("%d %d", &selling_price, &cost_price);
+    if (selling_price>cost_price)
     printf("Profit\n");
-    else if (cost_price>=selling_price) 
+    else if (cost_price>selling_price) 
     printf("Loss\n");
     else
     printf("No Profit No Loss\n");
