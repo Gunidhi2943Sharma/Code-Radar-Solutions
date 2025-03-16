6c4915ba9849bf8ec2a30b345c582e6f19e9609b
Gunidhi Sharma <gunidhi2943sharma@gmail.com>
2025-03-16 19:12:02 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,a=0,b=1,c,i;
+    printf("");
+    scanf("%d",&n);
+    for(i=0;i<n;i++) {
+        c=a+b;
+        a=b;
+        b=c;
+        if (a%3==0){
+            continue;
+        }
+        else{
+            printf("%d\n", a);
+            }
+    }
+    return 0;
+}
